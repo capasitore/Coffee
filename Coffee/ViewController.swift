b5f0ed89da1aa65385015d8e7d9f914d337a90f3
capasitore <aronsson@gmail.com>
2015-12-07 13:28:13 +0100

    Initial Commit

---
+++ Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by Andreas on 07/12/15.
+//  Copyright © 2015 Andreas Aronsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
